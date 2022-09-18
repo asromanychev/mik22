f3a13a27897204deac9aaab2cf964ef7f12956d8
Andrey Romanychev <andrey.romanychev@insales.ru>
2022-09-18 20:55:04 +0300

    add js and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_20_172223) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_18_083603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +72,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_20_172223) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.string "pincode"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
@@ -73,6 +85,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_20_172223) do
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_photos_on_event_id"
     t.index ["user_id"], name: "index_photos_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "quotes", force: :cascade do |t|
