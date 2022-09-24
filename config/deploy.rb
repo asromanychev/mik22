fde0a8a7bc1961742ad460f79d1dc0e955f95595
Andrey Romanychev <andrey.romanychev@insales.ru>
2022-09-24 07:27:06 +0300

    test on cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,4 +6,69 @@ set :repo_url, "git@github.com:asromanychev/mik22.git"
 # Also works with non-github repos, I roll my own gitolite server
 set :deploy_to, "/home/deploy/#{fetch :application}"
 set :rbenv_prefix, '/usr/bin/rbenv exec' # Cf issue: https://github.com/capistrano/rbenv/issues/96
+
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+namespace :deploy do
+  # after :restart, :clear_cache do
+  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #     # Here we can do anything such as:
+  #     # within release_path do
+  #     #   execute :rake, 'cache:clear'
+  #     # end
+  #   end
+  # end
+  #
+  # task :restart do
+  # end
+
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
+      end
+    end
+    before :linked_files, :set_env_file do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/.env ]")
+          upload! '.env', "#{shared_path}/.env"
+        end
+      end
+    end
+  end
+
+  namespace :assets do
+    Rake::Task['deploy:assets:precompile'].clear_actions
+
+    desc "Precompile assets on local machine and upload them to the server."
+    task :precompile do
+      run_locally do
+        execute "mkdir -p ./public/assets"
+        execute "rm -r ./public/assets"
+      end
+
+      run_locally do
+        execute "RAILS_ENV=production bundle exec rake assets:precompile"
+        execute 'rm ./tmp/assets.tar.gz' rescue nil
+        execute 'tar -C ./public -zcvf ./tmp/assets.tar.gz assets'
+      end
+
+      on roles(:web) do
+        within release_path do
+          public_path = "#{release_path}/public"
+
+          upload! "./tmp/assets.tar.gz", public_path
+
+          execute "tar -C #{public_path} -zxvf #{public_path}/assets.tar.gz"
+        end
+      end
+
+      run_locally do
+        execute "rm -r ./public/assets"
+        execute 'rm ./tmp/assets.tar.gz' rescue nil
+      end
+    end
+  end
+end
