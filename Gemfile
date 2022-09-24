4e1069eae21f22f69d6bcf83e2b88d1c2f4ae596
Andrey Romanychev <andrey.romanychev@insales.ru>
2022-09-24 08:56:42 +0300

    remove sprockets

--- Gemfile
+++ Gemfile
@@ -3,13 +3,13 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.0.3"
 gem "rails", "~> 7.0.2"
-gem "sprockets-rails"
+# gem "sprockets-rails"
 gem "pg", "~> 1.1"
 gem "puma", "~> 5.0"
 gem "importmap-rails"
 gem "jsbundling-rails"
 gem "turbo-rails", '~> 1.0.0'
-gem "sass-rails"
+gem "sassc-rails"
 gem "stimulus-rails"
 gem "cssbundling-rails"
 gem "jbuilder"
--- config/deploy.rb
+++ config/deploy.rb
@@ -6,5 +6,5 @@ set :repo_url, "git@github.com:asromanychev/mik22.git"
 # Also works with non-github repos, I roll my own gitolite server
 set :deploy_to, "/home/deploy/#{fetch :application}"
 set :rbenv_prefix, '/usr/bin/rbenv exec' # Cf issue: https://github.com/capistrano/rbenv/issues/96
-
+set :bundle_flags, ''
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
