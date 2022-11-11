4dbec5853a596580c46a8e6e4619e091f85c74b9
Andrey Romanychev <andrey.romanychev@insales.ru>
2022-11-11 15:22:11 +0300

    stimulus for form fields

--- app/javascript/controllers/vah_met_search_controller.js
+++ app/javascript/controllers/vah_met_search_controller.js
@@ -13,4 +13,28 @@ export default class extends Controller {
             }
         );
     }
+
+    openProgramSelect(e) {
+        [...document.getElementsByClassName('vah-met-program-name')].map(
+            (element, index, array) => {
+                $(element.firstChild)[0].className += " show";
+            }
+        );
+    }
+
+    openOtherSelects(e) {
+        [...document.getElementsByClassName('vah-met-parameter-name')].map(
+            (element, index, array) => {
+                $(element.firstChild)[0].className += " show";
+            }
+        );
+        [...document.getElementsByClassName('vah-met-lot-packet'),
+            ...document.getElementsByClassName('vah-met-lot-order'),
+            ...document.getElementsByClassName('vah-met-wafer-number')
+        ].map(
+            (element, index, array) => {
+                $(element)[0].classList.remove("collapse");
+            }
+        );
+    }
 }
